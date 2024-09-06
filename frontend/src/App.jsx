0f3bb6f036bd45da54d34842642b464ad6650625
rbo13 <chardyy.orofeo@gmail.com>
2024-09-06 16:38:41 +0800

    initial project bootstrap for frontend directory
    Signed-off-by: rbo13 <chardyy.orofeo@gmail.com>

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Button } from 'antd'
+
+function App() {
+
+  return (
+    <>
+      <Button type='primary'>Hello, world</Button>
+    </>
+  )
+}
+
+export default App
