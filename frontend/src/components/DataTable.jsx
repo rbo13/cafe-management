5ee59b782d5114a2adfcedcd02699f48185810c8
rbo13 <chardyy.orofeo@gmail.com>
2024-09-08 16:13:58 +0800

    added search functionality
    Signed-off-by: rbo13 <chardyy.orofeo@gmail.com>

--- frontend/src/components/DataTable.jsx
+++ frontend/src/components/DataTable.jsx
@@ -1,9 +1,12 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo, useRef } from 'react'
 import { AgGridReact } from 'ag-grid-react'
-import "ag-grid-community/styles/ag-grid.css";
-import "ag-grid-community/styles/ag-theme-quartz.css";
-import { PlusOutlined } from '@ant-design/icons';
-import DataTableHeader from './DataTableHeader';
+import "ag-grid-community/styles/ag-grid.css"
+import "ag-grid-community/styles/ag-theme-quartz.css"
+import { Input } from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
+import DataTableHeader from './DataTableHeader'
+
+const { Search } = Input
 
 const DataTable = ({
   data = [],
@@ -17,6 +20,8 @@ const DataTable = ({
   headerTitle,
   buttonText
 }) => {
+  const gridRef = useRef(null)
+
   const defaultGridOptions = useMemo(() => ({
     pagination: true,
     paginationPageSize: 10,
@@ -28,24 +33,44 @@ const DataTable = ({
     suppressDragLeaveHidesColumns: true,
   }), [])
 
-  const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
-  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+  const containerStyle = useMemo(() => ({ width: "100%", height: "100%", padding: "16px" }), [])
+  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), [])
+
   const defaultColDef = useMemo(() => {
     return {
       flex: 10,
-    };
-  }, []);
+      filter: true
+    }
+  }, [])
 
   const mergedGridOptions = useMemo(() => ({
     ...defaultGridOptions,
     ...gridOptions,
   }), [defaultGridOptions, gridOptions])
 
+  const onFilterTextBoxChanged = useCallback(() => {
+    gridRef.current.api.setGridOption(
+      "quickFilterText",
+      (document.getElementById("search")).value,
+    );
+  }, []);
+
   if (isLoading) return <p>Loading...</p>
   if (error) return <p>Error: {error.message}</p>
 
   return (
     <div style={containerStyle}>
+      <div style={{ marginBottom: '16px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <div style={{ display: 'flex', gap: '16px' }}>
+          <Search
+            id='search'
+            placeholder="Filter by Location"
+            onChange={onFilterTextBoxChanged}
+            style={{ width: 300 }}
+            enterButton
+          />
+        </div>
+      </div>
       <DataTableHeader
         title={headerTitle}
         onAdd={onAdd}
@@ -58,6 +83,7 @@ const DataTable = ({
         className="ag-theme-quartz"
       >
         <AgGridReact
+          ref={gridRef}
           rowData={data}
           columnDefs={columnDefs}
           defaultColDef={defaultColDef}
