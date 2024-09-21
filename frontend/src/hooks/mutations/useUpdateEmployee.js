c4ab9d1d4b47eac48b167a95cde373269b428318
rbo13 <chardyy.orofeo@gmail.com>
2024-09-21 14:36:39 +0800

    fixed redirect
    Signed-off-by: rbo13 <chardyy.orofeo@gmail.com>

--- frontend/src/hooks/mutations/useUpdateEmployee.js
+++ frontend/src/hooks/mutations/useUpdateEmployee.js
@@ -1,13 +1,18 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useNavigate } from '@tanstack/react-router'
 import { updateEmployee } from '../../api/employee'
 
 const useUpdateEmployee = () => {
   const queryClient = useQueryClient()
+  const navigate = useNavigate()
 
   return useMutation({
     mutationFn: updateEmployee,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['employee', 'employees'] })
+      navigate({
+        to: '/employees'
+      })
     },
     onError: (error) => {
       console.error('Error updating employee:', error.message)
